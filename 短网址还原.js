c67b8343d68b32c0e743d0b8881e7149941b9650
callacat <62171226+callacat@users.noreply.github.com>
2023-08-20 23:40:19 +0800

    Update 短网址还原.js
    取消需要申请api的限制

--- 短网址还原.js
+++ 短网址还原.js
@@ -2,25 +2,19 @@
  * @author Dswang
  * @origin Dswang
  * @name 短网址还原
- * @version 1.0.0
- * @description 获取短网址缩短之前的网址。基本支持所有短网址（不支持国内打不开的网址）
+ * @version 1.1.0
+ * @description 获取短网址缩短之前的网址。基本支持所有短网址
  * @rule ^网址还原 (.+)$
  * @priority 99999
  * @admin false
  */
 
-/*
-请先访问https://admin.alapi.cn/dashboard/ 获取token，然后通过 set ALAPI Token ? 设置
-*/
-
-const axios = require('axios');
+// 引入request模块
+const request = require('request');
 
 module.exports = async (s) => {
   
-  const ALAPIStorage = new BncrDB('ALAPI');
-  const ALAPIToken = await ALAPIStorage.get('Token');
-  if (!ALAPIToken) return s.reply("请使用命令'set ALAPI Token ?,设置ALAPI的Token");
-
+  // 获取用户输入的短网址
   const shortUrl = s.param(1);
 
   if (!shortUrl) {
@@ -29,19 +23,19 @@ module.exports = async (s) => {
   }
 
   try {
-    const response = await axios.get('https://v2.alapi.cn/api/url/query', {
-      params: {
-        token: ALAPIToken,
-        url: shortUrl,
-      },
+    // 发送请求，并设置followRedirect为false，表示不自动跟随重定向
+    request({ url: shortUrl, followRedirect: false }, (err, res, body) => {
+      // 如果没有错误，并且响应状态码为301或302，表示有重定向
+      if (!err && (res.statusCode === 301 || res.statusCode === 302)) {
+        // 返回重定向的网址，即res.headers.location
+        s.reply(`还原后的网址：${res.headers.location}`);
+      } else {
+        // 如果有错误或者没有重定向，直接回复“短网址还原失败，请直接访问”
+        s.reply('短网址还原失败，请直接访问');
+      }
     });
-
-    if (response.data.code === 200) {
-      await s.reply(`还原后的网址：${response.data.data.long_url}`);
-    } else {
-      await s.reply('短网址还原失败，请稍后重试。');
-    }
   } catch (error) {
-    await s.reply('短网址还原失败，请稍后重试。');
+    // 如果发生错误，则返回错误信息
+    s.reply('短网址还原失败，请稍后重试。');
   }
 };
